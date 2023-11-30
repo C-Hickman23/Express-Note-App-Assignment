a53d03013c159d9557fe4dfacb4dc7dd5e2b80dd
Cardon Hickman <cardonhickman@gmail.com>
2023-11-30 16:25:29 -0700

    more bug fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,6 @@
 const router = require("express").Router();
 const { v4: uuid } = require("uuid");
 const fs = require("fs");
-const { json } = require("express");
 
 router.get("api/notes", async (req, res) => {
     const result = await JSON.parse(fs.readFileSync("db/db.json"));
@@ -9,15 +8,15 @@ router.get("api/notes", async (req, res) => {
 })
 
 router.post("api/notes", async (req, res) => {
-    const post = await JSON.parse(fs.readFileSync("db/db.json"))
+    const postData = await JSON.parse(fs.readFileSync("db/db.json"))
     const newNote = {
         title: req.body.title,
         text: req.body.text,
         id: uuid(),
     };
-    dbJson.push(newNote);
-    fs.writeFileSync("db/db.json", JSON.stringify(post));
-    res.json(post);
+    postData.push(newNote);
+    fs.writeFileSync("db/db.json", JSON.stringify(postData));
+    res.json(postData);
 })
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,7 @@ const path = require("path");
 routes.get("/", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"))
 });
+
 routes.get("/notes", (req,res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"))
 });
